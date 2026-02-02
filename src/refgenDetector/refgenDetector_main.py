8f79b5b757ef9ec34e9b0f0609a1f3ed94eb4d3d
Mireia Marin I Ginestar <100416435+mirgin01@users.noreply.github.com>
2026-02-02 16:34:27 +0100

    Update refgenDetector_main.py

--- src/refgenDetector/refgenDetector_main.py
+++ src/refgenDetector/refgenDetector_main.py
@@ -24,7 +24,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
-from .reference_genome_dictionaries import *
+from reference_genome_dictionaries import *
 from exceptions.NoFileException import *
 from aligment_files import *
 from variant_files import *
