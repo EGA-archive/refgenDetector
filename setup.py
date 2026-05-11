8fcd9216078d7ad2d99e5296f40f9218020257b0
Mireia Marin I Ginestar <mireiamaringinestar@gmail.com>
2026-05-11 17:41:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
-VERSION = '3.0.4'
+VERSION = '3.0.5'
 DESCRIPTION = 'RefgenDetector'
 
 
